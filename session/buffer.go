f2aa2721ee4dccb54af43523c28823b1832b8884
axelri <axel.riese@gmail.com>
2013-04-20 18:09:54 +0200

    some static things to the wiki

--- session/buffer.go
+++
@@ -1,7 +0,0 @@
-package session
-
-// Buffer describes a buffer of information that could be printed out
-// to the user. It could later be assigned to a certain GUI tab.
-type Buffer struct {
-	// TODO
-}
