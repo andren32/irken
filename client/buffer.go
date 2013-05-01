4d29e0bb76521d99eb8b56a7acedbe35cc87e3dc
andren32 <andren32@hotmail.com>
2013-05-01 18:10:47 +0200

    bufferfix, connectsession

--- client/buffer.go
+++ client/buffer.go
@@ -7,12 +7,14 @@ import (
 
 // Buffer describes a buffer of information that could be printed out
 // to the user. It could later be assigned to a certain GUI tab.
+// Can store nicks if the buffer is for a channel
 type Buffer struct {
-	buf bytes.Buffer
+	buf   bytes.Buffer
+	nicks string
 }
 
 // Write writes a string to the buffer. Returns error if something goes wrong
-func (b *Buffer) Write(str string) err {
+func (b *Buffer) Write(str string) error {
 	_, err := b.buf.WriteString(str)
 	return err
 }
@@ -21,9 +23,9 @@ func (b *Buffer) Write(str string) err {
 // Returns an error if the buffer is empty.
 func (b *Buffer) Flush() (string, error) {
 	s := b.buf.String()
-	if s == nil {
-		return nil, errors.New("Buffer is empty.")
+	if s == "" {
+		return "", errors.New("Buffer is empty.")
 	}
-	b.buffer.Reset()
+	b.buf.Reset()
 	return s, nil
 }
